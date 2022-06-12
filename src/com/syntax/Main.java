fca11694444e5e73cf5c380b5c6ee81c93bf737e
hilalparlak2 <hilalceit@gmailcom>
2022-06-11 23:53:48 -0500

    Hilale yardim

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,11 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String course = "Esengul, Yasemin, Figen";
+
+        System.out.println("if you are work well all, " + course + " will find very good job");
+    }
+}
